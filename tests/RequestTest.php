5e9695aa0a3778ed4daaa5d108845c0ced16e91d
Brian Webb <bwebb@indatus.com>
2014-01-23 09:15:44 -0500

    remove build breaking test

--- tests/RequestTest.php
+++ tests/RequestTest.php
@@ -7,7 +7,7 @@ class RequestTest extends TruckerTests
 
     public function testParseResponseToData()
     {
-        $this->assertEquals(1,2, 'My test broke the build');
+
     }
 
     public function testSetTransportLanguage()
