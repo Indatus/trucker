14b4b8cf72620dc9d0e3ad13be6f2698ffc23245
Damien Russell <drussell@indatus.com>
2014-05-29 14:36:14 -0400

    changed finders to use a fully qualified name for instantiating new instances of models

--- src/Trucker/Finders/InstanceFinder.php
+++ src/Trucker/Finders/InstanceFinder.php
@@ -86,8 +86,8 @@ class InstanceFinder
 
         //kraft the response into an object to return
         $data     = $response->parseResponseToData();
-        $klass    = $model->getResourceName();
-        $instance = new $klass($data);
+        $instance = new $model($data);
+        
 
         //inflate the ID property that should be guarded
         $id = $instance->getIdentityProperty();
