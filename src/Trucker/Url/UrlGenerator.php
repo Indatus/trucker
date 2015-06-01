73a69adccf6a0597320489f316da7081a2aceb74
Erwin Kooi <ek@cdyweb.com>
2015-05-31 21:19:03 -0400

    Use the path_prefix config option while building the API path

--- src/Trucker/Url/UrlGenerator.php
+++ src/Trucker/Url/UrlGenerator.php
@@ -12,6 +12,7 @@ namespace Trucker\Url;
 
 use Illuminate\Container\Container;
 use Doctrine\Common\Inflector\Inflector;
+use Trucker\Facades\Config;
 
 class UrlGenerator
 {
@@ -172,6 +173,7 @@ class UrlGenerator
             $uri = implode("/", $uriResult) . "/$uri";
         }
 
-        return "/$uri";
+        $prefix = Config::get('request.path_prefix','/');
+        return "{$prefix}{$uri}";
     }
 }
